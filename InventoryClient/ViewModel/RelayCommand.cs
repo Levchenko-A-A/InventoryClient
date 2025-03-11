fc214067135443e940866f0df950e929bdffdbd5
Андрей Левченков <lev_a_a@mail.ru>
2025-03-11 19:26:48 +0200

    1212

--- InventoryClient/ViewModel/RelayCommand.cs
+++ InventoryClient/ViewModel/RelayCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace InventoryClient.ViewModel
+namespace InventoryClient
 {
     class RelayCommand : ICommand
     {
