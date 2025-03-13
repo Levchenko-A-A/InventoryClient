17f6ff16d4a199fb28932952eabf7dbda30278d8
Андрей Левченков <lev_a_a@mail.ru>
2025-03-13 20:39:28 +0200

    20:39

--- InventoryClient/ViewModel/ManufacturerViewModel.cs
+++ InventoryClient/ViewModel/ManufacturerViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -39,49 +40,65 @@ namespace InventoryClient.ViewModel
                 OnPropertyChanged(nameof(Manufacturers));
             }
         }
-        private Person? selectedPerson;
-        public Person? SelectedPerson
+        private Manufacturer? selectedManufacturer;
+        public Manufacturer? SelectedManufacturer
         {
-            get => selectedPerson;
+            get => selectedManufacturer;
             set
             {
-                selectedPerson = value;
-                OnPropertyChanged(nameof(SelectedPerson));
+                selectedManufacturer = value;
+                OnPropertyChanged(nameof(SelectedManufacturer));
             }
         }
 
-        //private RelayCommand addCommand;
-        //public RelayCommand AddCommand
-        //{
-        //    get
-        //    {
-        //        return addCommand ?? (addCommand = new RelayCommand(async obj =>
-        //        {
-        //            PersonWindow personWindow = new PersonWindow(new Person());
-        //            if (personWindow.ShowDialog() == true)
-        //            {
-        //                await sendClient(personWindow.Person);
-        //            }
-        //        }));
-        //    }
-        //}
-
-        //private RelayCommand deleteCommand;
-        //public RelayCommand DeleteCommand
-        //{
-        //    get
-        //    {
-        //        return deleteCommand ?? (deleteCommand = new RelayCommand(async (selectedItem) =>
-        //        {
-        //            Person? client = selectedItem as Person;
-        //            if (client == null) return;
-        //            if (MessageBox.Show("Вы действительно хотите удалить элемент?", "Внимание", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
-        //            {
-        //                await delPerson(client.Personid);
-        //            }
-        //        }));
-        //    }
-        //}
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(async obj =>
+                {
+                    ManufacturerWindow manufacturerWindow = new ManufacturerWindow(new Manufacturer());
+                    if (manufacturerWindow.ShowDialog() == true)
+                    {
+                        await sendManufacturer(manufacturerWindow.Manufacturer);
+                    }
+                }));
+            }
+        }
+        private RelayCommand updateCommand;
+        public RelayCommand UpdateCommand
+        {
+            get
+            {
+                return updateCommand ?? (updateCommand = new RelayCommand(async (selectedItem) =>
+                {
+                    Manufacturer? manufacturer = selectedItem as Manufacturer;
+                    if (manufacturer == null) return;
+                    ManufacturerWindow manufacturerWindow = new ManufacturerWindow(manufacturer);
+                    if (manufacturerWindow.ShowDialog() == true)
+                    {
+                        //await UpdateManufacturer(manufacturerWindow.Manufacturer);
+                    }
+                }));
+            }
+        }
+        private RelayCommand deleteCommand;
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ?? (deleteCommand = new RelayCommand(async (selectedItem) =>
+                {
+                    Manufacturer? manufacturer = selectedItem as Manufacturer;
+                    if (manufacturer == null) return;
+                    if (MessageBox.Show("Вы действительно хотите удалить элемент?", "Внимание", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK)
+                    {
+                        await delPerson(manufacturer.Manufacturerid);
+                    }
+                }));
+            }
+        }
         private async Task<ObservableCollection<Manufacturer>> getManufacturer()
         {
             try
@@ -109,6 +126,61 @@ namespace InventoryClient.ViewModel
             {
                 MessageBox.Show($"Неизвестная ошибка: {ex.Message}");
                 return new ObservableCollection<Manufacturer>();
+            }
+        }
+
+        private async Task sendManufacturer(Manufacturer manufacturer)
+        {
+            try
+            {
+                JsonContent content = JsonContent.Create(manufacturer);
+                var request = new HttpRequestMessage(HttpMethod.Post, "http://127.0.0.1:8888/connection/");
+                request.Content = content;
+                request.Headers.Add("table", "manufacturer");
+                using var response = await httpClient.SendAsync(request);
+                string responseText = await response.Content.ReadAsStringAsync();
+                if (responseText == "Error")
+                    MessageBox.Show("Производитель с таким именем существует");
+                else if (responseText == "OK")
+                {
+                    MessageBox.Show("Производитель добавлен");
+                    Load();
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Ошибка HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+        public async Task delPerson(int clientId)
+        {
+            try
+            {
+                JsonContent content = JsonContent.Create(clientId);
+                var request = new HttpRequestMessage(HttpMethod.Delete, "http://127.0.0.1:8888/connection/");
+                request.Content = content;
+                request.Headers.Add("table", "manufacturer");
+                using var response = await httpClient.SendAsync(request);
+                string responseText = await response.Content.ReadAsStringAsync();
+                if (responseText == "Error")
+                    MessageBox.Show("Пользователь с таким именем не существует");
+                else if (responseText == "OK")
+                {
+                    MessageBox.Show("Пользователь удален");
+                    Load();
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Ошибка HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
             }
         }
     }
