6455065d5d9b38fa0e20c8201c3a7d477460a3dc
Андрей Левченков <lev_a_a@mail.ru>
2025-03-15 16:35:05 +0200

    Personrole

--- InventoryClient/Model/Personrole.cs
+++ InventoryClient/Model/Personrole.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace InventoryClient.Model
 {
     public class Personrole
     {
+        [JsonPropertyName("userroleid")]
         public int Userroleid { get; set; }
-
+        [JsonPropertyName("userid")]
         public int? Userid { get; set; }
-
+        [JsonPropertyName("roleid")]
         public int? Roleid { get; set; }
-
+        [JsonPropertyName("role")]
         public virtual Role? Role { get; set; }
-
+        [JsonIgnore]
         public virtual Person? User { get; set; }
     }
 }
+
