984a96d5d5047fce42cf4bfbb4e6d772adc8cb0e
Grom76s <grighorii.krist.76@gmail.com>
2025-03-21 20:18:12 +0200

    2103

--- InventoryClient/View/DeviceWindow.xaml.cs
+++ InventoryClient/View/DeviceWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using InventoryClient.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,11 +24,46 @@ namespace InventoryClient.View
     public partial class DeviceWindow : Window
     {
         public Device device { get; set; }
+        private HttpClient httpClient;
+        public List<Category> Categories { get; set; } =new();
         public DeviceWindow(Device dec)
         {
             InitializeComponent();
+            httpClient = new HttpClient();
             device = dec;
             DataContext = device;
+            Task<List<Category>> task = Task.Run(() => getLocation());
+            Categories = task.Result;
+            ComboBoxCategory.ItemsSource = Categories;
+        }
+        private async Task<List<Category>> getLocation()
+        {
+            try
+            {
+                StringContent content = new StringContent("getCategoryAll");
+                using var request = new HttpRequestMessage(HttpMethod.Get, "http://193.104.57.148:8080/connection/");
+                request.Headers.Add("table", "category");
+                request.Content = content;
+                using HttpResponseMessage response = await httpClient.SendAsync(request);
+                string responseText = await response.Content.ReadAsStringAsync();
+                List<Category> clients = JsonSerializer.Deserialize<List<Category>>(responseText)!;
+                return clients;
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Ошибка HTTP-запроса: {ex.Message}");
+                return new List<Category>();
+            }
+            catch (JsonException ex)
+            {
+                MessageBox.Show($"Ошибка десериализации JSON: {ex.Message}");
+                return new List<Category>();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Неизвестная ошибка: {ex.Message}");
+                return new List<Category>();
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
